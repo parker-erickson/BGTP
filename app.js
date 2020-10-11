3482166de5d4929f6e41f9174b7b5e909a10f42d
Parker <parkererickson.software@gmail.com>
2020-10-10 18:08:20 -0700

    built a basic login and register page setup

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+//connect to database
+require('./database/connection')
+require('./public/javascript/userAccounts')
+require('./public/javascript/errors')
 
-//added-----------
 
-const bcrypt = require('bcrypt')
-const passport = require('passport')
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-// const flash = require('express-flash') I will use these in the login and register
-// const session = require('express-session')
-// const methodOverride = require('method-override')
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -20,13 +17,9 @@ const cartRouter = require('./routes/cart');
 const productsRouter = require('./routes/products');
 const profileRouter = require('./routes/profile');
 const postRouter = require('./routes/post');
-const signupRouter = require('./routes/signup');
+const registerRouter = require('./routes/register');
 
 const app = express();
-
-require('./database/connection')
-
-//--------------------
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -45,90 +38,13 @@ app.use('/users', usersRouter);
 app.use('/cart', cartRouter);
 app.use('/post', postRouter);
 app.use('/products',productsRouter);
-app.use('/signup',signupRouter);
+app.use('/register',registerRouter);
 app.use('/profile',profileRouter);
-
-
-// GET actions
-
-app.get('/', checkAuthenticated, (req, res) =>{
-  res.render('index.ejs', { name: req.user.name})
-})
-
-app.get('/login', checkNotAuthenticated, (req, res) => {
-  res.render('login.ejs')
-})
-
-app.get('/register', checkNotAuthenticated, (req, res) => {
-  res.render('register.ejs')
-})
-
-app.post('/register', checkNotAuthenticated, async(req,res) =>{
-  try{
-    const hashedPassword = await bcrypt.hash(req.body.password, 10)
-    users.push({
-      id: Date.now().toString(),
-      name: req.body.name,
-      email: req.body.email,
-      password: hashedPassword
-    })
-    res.redirect('/login')
-  } catch {
-    res.redirect('/register')
-  }
-})
-
-app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/login',
-  failureFlash: true
-}))
-
-
-app.delete('/logout', (req, res) => {
-  req.logOut()
-  res.redirect('/login')
-})
-
-function checkAuthenticated(req, res, next) {
-  if(req.isAuthenticated()){
-    return next()
-  }
-
-  res.redirect('/login')
-}
-
-function checkNotAuthenticated(req, res, next) {
-  if(req.isAuthenticated()){
-    return res.redirect('/')
-  }
-
-  next()
-}
 
 //port wiring
 //opening server and opening listening channel
 app.listen(8081, function() {
   //opens server on port 3000, does stuff
-});
-
-
-//---------
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
 });
 
 module.exports = app;
--- database/connection.js
+++ database/connection.js
@@ -1,4 +1,7 @@
 const mysql = require('mysql')
+const dotenv = require('dotenv')
+
+dotenv.config({ path: './.env' })
 
 let connection
 if (process.env.JAWSDB_URL) {
@@ -8,13 +11,19 @@ if (process.env.JAWSDB_URL) {
     // create connection
     console.log("local db")
     connection =  mysql.createConnection({
-        host: 'localhost'	,
-        user: 'root',
-        password: 'carcassonne',
-        database: 'bgtp',
+        host: process.env.DATABASE_HOST,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE
     });
 }
 
-connection.connect();
+connection.connect( (err) => {
+    if(err) {
+        console.log('Error connecting to database: ' + err)
+    } else {
+        console.log('mysql connected...')
+    }
+});
 
 module.exports = connection
